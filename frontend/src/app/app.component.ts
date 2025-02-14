0b4ca9c71a880ae6e4610e6b529bcecab5c8417f
Melisa Cihan <melisa-cihan@outlook.de>
2025-02-14 23:10:29 +0100

    bind footer into app and change color

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, NavComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
