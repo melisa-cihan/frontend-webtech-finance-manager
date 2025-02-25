c232ce3458f3d3e01273f8c355953cebc46eb0d0
Melisa Cihan <melisa-cihan@outlook.de>
2025-02-25 16:43:41 +0100

    implement asset.ts interface

--- frontend/src/app/shared/asset.ts
+++ frontend/src/app/shared/asset.ts
@@ -1,2 +1,10 @@
 export interface Asset {
+    id: string;
+    asset: string;
+    category: string;
+    current_value: string;
+    purchase_price: number;
+    roi: number;
+    location: string;
+    purchase_date: Date;
 }
