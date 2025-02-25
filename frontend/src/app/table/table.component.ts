83b88db5ba9c172bd377169839dd85802b7ce8e0
Melisa Cihan <melisa-cihan@outlook.de>
2025-02-25 17:04:17 +0100

    bind service into table component

--- frontend/src/app/table/table.component.ts
+++ frontend/src/app/table/table.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { BackendService } from '../shared/backend.service';
+import { Asset } from '../shared/asset';
+import { FormControl } from '@angular/forms';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-table',
@@ -7,6 +11,39 @@ import { Component } from '@angular/core';
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
-export class TableComponent {
+export class TableComponent implements OnInit{
+  asset!: Asset[];
+  search = new FormControl();
+
+  constructor(private bs: BackendService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.readAll();
+  }
+
+  readAll(): void {
+    this.bs.getAllAssets().subscribe(
+      {
+        next: (response) => {
+              this.asset = response;
+              console.log(this.asset);
+              return this.asset;
+            },
+        error: (err) => console.log(err),
+        complete: () => console.log('getAll() completed')
+      })
+}
+  delete(id: string): void {
+    console.log('id', id)
+    this.bs.deleteOneAsset(id).subscribe(
+        {
+          next: (response) => {
+                console.log(response);
+                this.readAll();
+              },
+          error: (err) => console.log(err),
+          complete: () => console.log('deleting completed')
+        })
+  }
 
 }
