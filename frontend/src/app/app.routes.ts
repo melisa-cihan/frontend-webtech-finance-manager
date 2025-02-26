90ade35c3ee0a8be75f6d60062a1af1a8a14fa89
Melisa Cihan <melisa-cihan@outlook.de>
2025-02-26 19:24:34 +0100

    add dashboard path to app routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TableComponent } from './table/table.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
     {
@@ -12,4 +13,9 @@ export const routes: Routes = [
         path: 'assets', 
         component: TableComponent
     },
+    {
+        path: 'dashboard', 
+        component: DashboardComponent
+    },
+    
 ];
