0acffb9a1d700dbe9a855e4bedd45b02721d0eac
Melisa Cihan <melisa-cihan@outlook.de>
2025-02-27 11:56:46 +0100

    add http to app config + implement read all method for asset component and html table

--- frontend/src/app/assets/assets.component.ts
+++ frontend/src/app/assets/assets.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+import { FormControl, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { Asset } from '../shared/asset';
+import { BackendService } from '../shared/backend.service';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-assets',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './assets.component.html',
   styleUrl: './assets.component.css'
 })
-export class AssetsComponent {
+export class AssetsComponent implements OnInit {
+  asset!: Asset[];
+  search = new FormControl();
+  constructor(private bs: BackendService, private router: Router) { }
 
+  ngOnInit(): void {
+    this.readAll()
+  }
+
+  readAll(): void {
+    this.bs.getAllAssets().subscribe(
+      {
+        next: (response) => {
+              this.asset = response;
+              console.log(this.asset);
+              return this.asset;
+            },
+        error: (err) => console.log(err),
+        complete: () => console.log('getAll() completed')
+      })
+  }
+  
 }
