01bd314a8d1a4285c426e3af4acfc819c9081271
Melisa Cihan <melisa-cihan@outlook.de>
2025-03-09 16:04:29 +0100

    add create path to routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AssetsComponent } from './assets/assets.component';
 import { DetailComponent } from './detail/detail.component';
+import { CreateComponent } from './create/create.component';
 
 export const routes: Routes = [{
         path: "",
@@ -20,5 +21,9 @@ export const routes: Routes = [{
       {
         path: "assets/:id",
         component: DetailComponent
+      },
+      {
+        path: "create",
+        component: CreateComponent
       }
 ];
